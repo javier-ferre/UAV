fe8a85158ac8c5b037b63d850a264ebfff0f3f53
javier-ferre <j.ferre@alumnos.upm.es>
2021-07-04 22:07:31 +0200

    Added saturation to the PID controller

--- controller/PID.cpp
+++ controller/PID.cpp
@@ -4,6 +4,8 @@ double PID::compute(double setpoint, double current)
 {
     diff = setpoint - current;
     sum += diff;
-
-    return (Kp*diff+Ki*sum+Kd*(diff-prev));
+    double result = (Kp*diff+Ki*sum+Kd*(diff-prev));
+    if (result > saturationMax) return saturationMax;
+    if (result < saturationMin) return saturationMin;
+    return result;
 }
--- controller/PID.hpp
+++ controller/PID.hpp
@@ -9,8 +9,10 @@ private:
     double diff;
     double sum;
     double prev;
+    double saturationMin;
+    double saturationMax;
 
 public:
-    PID(double Kp, double Ki, double Kd) : Kp{Kp}, Ki{Ki}, Kd{Kd}, sum{0}, diff{0}, prev{0} {};
+    PID(double Kp, double Ki, double Kd, double saturationMax, double saturationMin) : Kp{Kp}, Ki{Ki}, Kd{Kd}, sum{0}, diff{0}, prev{0}, saturationMin{saturationMin}, saturationMax{saturationMax} {};
     double compute(double setpoint, double current);
 };
