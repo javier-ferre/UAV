2205d299d204f1bce2cccb481eaf985ef85f3ea5
javier-ferre <j.ferre@alumnos.upm.es>
2021-07-07 13:28:38 +0200

    Changed test to use the new controllers

--- test.cpp
+++ test.cpp
@@ -1,35 +1,12 @@
 #include "./platform/GPS.hpp"
 #include "./platform/Motors.hpp"
 #include "./platform/IMU.hpp"
-#include "./controller/PID.hpp"
+#include "./controller/PositionController.hpp"
+#include "./controller/AttitudeController.hpp"
 #include "./estimator/KalmanFilter.hpp"
 
 #include <chrono>
 #include <iostream>
-
-const double zKp = 25;
-const double zKi = 0.008;
-const double zKd = 0;
-const double zSatMax = 50;
-const double zSatMin = -50;
-
-const double rollKp = 0.1;
-const double rollKi = 0;
-const double rollKd = 0;
-const double rollSatMax = 10;
-const double rollSatMin = -10;
-
-const double pitchKp = 0.1;
-const double pitchKi = 0;
-const double pitchKd = 0;
-const double pitchSatMax = 10;
-const double pitchSatMin = -10;
-
-const double yawKp = 0.03;
-const double yawKi = 0.000002;
-const double yawKd = 0;
-const double yawSatMax = 10;
-const double yawSatMin = -10;
 
 int main(int _argc, char **_argv)
 {
@@ -38,17 +15,24 @@ int main(int _argc, char **_argv)
   IMU imuSensor;
   Motors motors;
 
+  double minThrottle = 350;
   double throttle = 0;
-  double setpoint = 3;
-  double pitchSetpoint = 0;
+
+  double xSetpoint = 3;
+  double ySetpoint = 5;
+  double zSetpoint = 3;
+
   double rollSetpoint = 0;
+  double pitchSetpoint = 0;
   double yawSetpoint = 0;
 
-  PID rollPid(rollKp, rollKi, rollKd, rollSatMax, rollSatMin);
-  PID pitchPid(pitchKp, pitchKi, pitchKd, pitchSatMax, pitchSatMin);
-  PID yawPid(yawKp, yawKi, yawKd, yawSatMax, yawSatMin);
+  double throttleBackLeft = 0;
+  double throttleBackRight = 0;
+  double throttleFrontLeft = 0;
+  double throttleFrontRight = 0;
 
-  PID altitudePid(zKp, zKi, zKd, zSatMax, zSatMin);
+  PositionController positionController;
+  AttitudeController attitudeController;
 
   boost::qvm::mat<float,6,1> gpsMeasurements;
   boost::qvm::mat<float,3,1> imuMeasurements;
@@ -60,7 +44,7 @@ int main(int _argc, char **_argv)
   boost::qvm::A40(gpsMeasurements) = gpsSensor.getVelocityNorth();
   boost::qvm::A50(gpsMeasurements) = gpsSensor.getVelocityUp();
 
-  KalmanFilter uavEKF(gpsMeasurements);
+  KalmanFilter EKF(gpsMeasurements);
   auto end = std::chrono::high_resolution_clock::now();
   auto begin = std::chrono::high_resolution_clock::now();
 
@@ -77,24 +61,24 @@ int main(int _argc, char **_argv)
     boost::qvm::A10(imuMeasurements) = imuSensor.getAccelerationY();
     boost::qvm::A20(imuMeasurements) = imuSensor.getAccelerationZ();
 
-
     end = std::chrono::high_resolution_clock::now();
     auto elapsedTime = std::chrono::duration_cast<std::chrono::microseconds>(end-begin).count();
-    uavEKF.compute(imuMeasurements, gpsMeasurements, elapsedTime*0.000001, (imuSensor.getGyroRollAngle())*3.14159/180, (imuSensor.getGyroPitchAngle())*3.14159/180, (imuSensor.getGyroYawAngle())*3.14159/180);
+    EKF.compute(imuMeasurements, gpsMeasurements, elapsedTime*0.000001, (imuSensor.getGyroRollAngle())*3.14159/180, (imuSensor.getGyroPitchAngle())*3.14159/180, (imuSensor.getGyroYawAngle())*3.14159/180);
     begin = std::chrono::high_resolution_clock::now();
 
-    // std::cout << "Current altitude = " << gpsSensor.getAltitude() << std::endl;
-    // std::cout << "Current Yaw = " << imuSensor.getGyroYawAngle() << std::endl;
-    std::cout << "Esimated = " << uavEKF.getEstimatedX() << ", " << uavEKF.getEstimatedY() << ", " << uavEKF.getEstimatedZ() << std::endl;
-    std::cout << "  True   = " << gpsSensor.getX() << ", " << gpsSensor.getY() << ", " << gpsSensor.getZ() << std::endl;
+    std::cout << "EKF = " << EKF.getEstimatedX() << ", " << EKF.getEstimatedY() << ", " << EKF.getEstimatedZ() << std::endl;
+    std::cout << "GPS = " << gpsSensor.getX() << ", " << gpsSensor.getY() << ", " << gpsSensor.getZ() << std::endl;
+    std::cout << "IMU = " << imuSensor.getGyroRollAngle() << ", " << imuSensor.getGyroPitchAngle() << ", " << imuSensor.getGyroYawAngle() << std::endl;
 
-    throttle = 350 + altitudePid.compute(setpoint, gpsSensor.getAltitude());
-    motors.setMotorBackLeft(throttle+rollPid.compute(rollSetpoint,imuSensor.getGyroRollAngle())-pitchPid.compute(pitchSetpoint,imuSensor.getGyroPitchAngle())+yawPid.compute(yawSetpoint,imuSensor.getGyroYawAngle()));
-    motors.setMotorBackRight(throttle-rollPid.compute(rollSetpoint,imuSensor.getGyroRollAngle())-pitchPid.compute(pitchSetpoint,imuSensor.getGyroPitchAngle())-yawPid.compute(yawSetpoint,imuSensor.getGyroYawAngle()));
-    motors.setMotorFrontLeft(throttle+rollPid.compute(rollSetpoint,imuSensor.getGyroRollAngle())+pitchPid.compute(pitchSetpoint,imuSensor.getGyroPitchAngle())-yawPid.compute(yawSetpoint,imuSensor.getGyroYawAngle()));
-    motors.setMotorFrontRight(throttle-rollPid.compute(rollSetpoint,imuSensor.getGyroRollAngle())+pitchPid.compute(pitchSetpoint,imuSensor.getGyroPitchAngle())+yawPid.compute(yawSetpoint,imuSensor.getGyroYawAngle()));
+    positionController.compute(xSetpoint, ySetpoint, zSetpoint, EKF.getEstimatedX(), EKF.getEstimatedY(), EKF.getEstimatedZ(), rollSetpoint, pitchSetpoint, yawSetpoint, throttle);
+    attitudeController.compute(rollSetpoint, pitchSetpoint, yawSetpoint, imuSensor.getGyroRollAngle(), imuSensor.getGyroPitchAngle(), imuSensor.getGyroYawAngle(), throttleBackLeft, throttleBackRight, throttleFrontLeft, throttleFrontRight);
 
-    //std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    motors.setMotorBackLeft(minThrottle+throttle+throttleBackLeft);
+    motors.setMotorBackRight(minThrottle+throttle+throttleBackRight);
+    motors.setMotorFrontLeft(minThrottle+throttle+throttleFrontLeft);
+    motors.setMotorFrontRight(minThrottle+throttle+throttleFrontRight);
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
   }
 
   return 0;
